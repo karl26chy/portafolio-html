6f2447f60ddf52ed69de68713017e319448b76d2
carlos charris yepes <>
2026-04-17 19:57:03 -0500

    update

---
+++ src/js/index.js
@@ -0,0 +1,8 @@
+
+
+alert("Hola soy carlos, bienvenido a mi portafolio");
+const boton = document.getElementById("btn-cambiar");
+const texto = document.getElementById("texto-sobremi");
+boton.addEventListener("click", function(){
+texto.innerText = "Ademas de estudiar administracion, estoy desarrollando habilidades en programacion y creando proyectos reales."
+})
